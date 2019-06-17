007e40b7de20fdd27a515b9653d02540c89b04cb
Işık Kaplan <isik.kaplan@hotmail.com>
2019-06-17 23:49:00 +0300

    flake-8

--- sourandperky/apps/sour_and_perky/models/__init__.py
+++ sourandperky/apps/sour_and_perky/models/__init__.py
@@ -3,6 +3,5 @@ from .entry import Entry
 from .event import Event
 from .title import Title
 from .title_channel import TitleChannel
-from .user_trophy import UserTrophy
 from .user import User
-
+from .user_trophy import UserTrophy
