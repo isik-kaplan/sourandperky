d69893ae506bcd0cfd54f1c7c2a9e426467f9642
Işık Kaplan <isik.kaplan@hotmail.com>
2019-06-18 23:54:06 +0300

    fix flake8 yet again

--- sourandperky/utils/models.py
+++ sourandperky/utils/models.py
@@ -9,4 +9,4 @@ def field_image_from_link(instance, field_name, image_name, image_url):
     img_temp.write(urlopen(image_url).read())
     img_temp.flush()
 
-    getattr(instance, field_name).save(image_name+'.jpg', File(img_temp))
+    getattr(instance, field_name).save(image_name + '.jpg', File(img_temp))
