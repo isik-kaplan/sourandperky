585a6b2eb9326290d73b580f3f5e8a78eb889b72
Işık Kaplan <isik.kaplan@hotmail.com>
2019-06-21 03:15:00 +0300

    rename serializer files

---
+++ sourandperky/apps/api/serializer/__init__.py
@@ -0,0 +1,6 @@
+from .entry import EntrySerializer
+from .event import EventSerializer
+from .title import TitleSerializer
+from .title_channel import TitleChannelSerializer
+from .user import UserSerializer
+from .user_trophy import UserTrophySerializer
