69df29cf3bb80c6afef09bbf60439e3ddafa2f11
Işık Kaplan <isik.kaplan@hotmail.com>
2019-06-24 00:24:55 +0300

    extend browsable api

--- sourandperky/apps/api/urls.py
+++ sourandperky/apps/api/urls.py
@@ -3,6 +3,10 @@ from django.urls import path, include
 from . import views
 from .router import RooterWithUrls
 from .views.authentication.urls import social_auth_urls
+from rest_auth.urls import urlpatterns as rest_auth_urls
+from rest_framework.routers import APIRootView
+from utils.urls import url_mapping
+from rest_auth.registration.urls import urlpatterns as rest_auth_registration_urls
 
 router = RooterWithUrls()
 router.register(r'entries', views.EntryViewSet)
@@ -13,14 +17,25 @@ router.register(r'events', views.EventViewSet)
 router.register(r'user_trophies', views.UserTrophyViewSet)
 router.register(r'notifications', views.NotificationViewSet)
 
+rest_auth_registration_urls += [
+    path('', APIRootView.as_view(api_root_dict=url_mapping(rest_auth_urls)), name='auth_registration'),
+]
+
+rest_auth_urls += [
+    path('registration/', include(rest_auth_registration_urls))
+]
+
+rest_auth_urls += [
+    path('', APIRootView.as_view(api_root_dict=url_mapping(rest_auth_urls)), name='auth'),
+]
+
 api_urls = [
     path('social_auth/', include(social_auth_urls)),
+    path('auth/', include(rest_auth_urls))
 ]
 
 router.register_extra_urls(api_urls)
 
 api_urls += [
-    path('auth/', include('rest_auth.urls')),
-    path('auth/registration/', include('rest_auth.registration.urls')),
     path('', include(router.urls))
 ]
