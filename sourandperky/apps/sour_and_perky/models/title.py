584dbeda537064b322f13fc8e2120aab426bba4b
Işık Kaplan <isik.kaplan@hotmail.com>
2019-07-01 04:35:58 +0300

    remove unused methods from title and allow blank on channels

--- sourandperky/apps/sour_and_perky/models/title.py
+++ sourandperky/apps/sour_and_perky/models/title.py
@@ -6,20 +6,12 @@ from .common import CommonFields, first_user_id
 
 class Title(CommonFields):
     text = models.CharField(max_length=50, null=True, unique=True)
-    channels = models.ManyToManyField('TitleChannel', related_name="titles")
+    channels = models.ManyToManyField('TitleChannel', related_name="titles", blank=True)
     creator = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         default=first_user_id,
-        on_delete=models.SET_DEFAULT
+        on_delete=models.SET_DEFAULT,
     )
 
     def __str__(self):
         return self.text
-
-    @property
-    def random_entry(self):
-        return self.entries.filter(readability=True).random().get()
-
-    @property
-    def has_readable_entries(self):
-        return self.entries.filter(readability=True).exists()
