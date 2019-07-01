1cd8a13e95bf7402640b5c43650ae0c098beec2e
Işık Kaplan <isik.kaplan@hotmail.com>
2019-07-01 12:03:52 +0300

    refactor urls

--- sourandperky/sourandperky/urls.py
+++ sourandperky/sourandperky/urls.py
@@ -14,11 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, re_path, include
+from django.urls import path, include
 from apps.api.urls import api_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    re_path(r'accounts/', include('allauth.urls')),
-    re_path(r'^api/', include(api_urls))
+    path(r'accounts/', include('allauth.urls')),
+    path(r'api/', include(api_urls))
 ]
